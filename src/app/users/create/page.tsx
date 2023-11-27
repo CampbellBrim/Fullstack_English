88aa5653805e99286c1a611d9066a4f85aa4de27
CampbellBrim <campbellbrim@gmail.com>
2023-11-27 20:46:08 -0300

    updated users

--- src/app/users/create/page.tsx
+++ src/app/users/create/page.tsx
@@ -9,46 +9,47 @@ export default function Page() {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    let response;
-    try {
-      const body = {
-        userName: userName,
-        email: email,
-      };
-      response = await fetch("/api/users", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(body),
-      });
-    } catch (error) {
-      console.error(error);
-    } finally {
-      Toast(response!.status);
-    }
-  };
+  // const handleSubmit = async (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   let response;
+  //   try {
+  //     const body = {
+  //       userName: userName,
+  //       email: email,
+  //     };
+  //     response = await fetch("/api/users", {
+  //       method: "POST",
+  //       headers: {"Content-Type": "application/json"},
+  //       body: JSON.stringify(body),
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //   } finally {
+  //     Toast(response!.status);
+  //   }
+  // };
   return (
-    <div className="flex flex-row justify-center">
-      <ToastContainer position="top-center" />
-      <form onSubmit={handleSubmit} className="form flex flex-col">
-        <label htmlFor="userName">username</label>
-        <input
-          className="input input-bordered"
-          id="userName"
-          type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}></input>
-        <label htmlFor="email">email</label>
-        <input
-          className="input input-bordered"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}></input>
-        <button type="submit" className="btn">
-          Submit
-        </button>
-      </form>
-    </div>
+    // <div className="flex flex-row justify-center">
+    //   <ToastContainer position="top-center" />
+    //   <form onSubmit={handleSubmit} className="form flex flex-col">
+    //     <label htmlFor="userName">username</label>
+    //     <input
+    //       className="input input-bordered"
+    //       id="userName"
+    //       type="text"
+    //       value={userName}
+    //       onChange={(e) => setUserName(e.target.value)}></input>
+    //     <label htmlFor="email">email</label>
+    //     <input
+    //       className="input input-bordered"
+    //       type="email"
+    //       value={email}
+    //       onChange={(e) => setEmail(e.target.value)}></input>
+    //     <button type="submit" className="btn">
+    //       Submit
+    //     </button>
+    //   </form>
+    // </div>
+    <p>page here</p>
   );
 }
