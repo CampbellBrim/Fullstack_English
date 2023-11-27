44ceffca6cadf2f72284211d5a403bed8870a7ae
CampbellBrim <campbellbrim@gmail.com>
2023-11-27 16:39:58 -0300

    updated .env

---
+++ prisma/migrations/20231127193547_initial_production_migration/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Lesson" ALTER COLUMN "title" SET DEFAULT 'Placeholder Title';
+
+-- AlterTable
+ALTER TABLE "LessonPlan" ALTER COLUMN "title" SET DEFAULT 'Placeholder Title';
