1607c03f922a06ef5ce9ddecb89cd589a7bdbb48
CampbellBrim <campbellbrim@gmail.com>
2023-11-27 11:58:17 -0300

    error in create/page

--- src/app/create/page/page.tsx
+++ src/app/create/page/page.tsx
@@ -3,20 +3,21 @@ import prisma from "../../../../prisma/db";
 import NavForCreate from "@/components/NavForCreate/NavForCreate";
 
 export default async function Page() {
-  async function getData() {
-    let lessons;
-    try {
-      lessons = await prisma.lesson.findMany();
-    } catch (error) {
-      console.error(error);
-    }
-    return lessons;
-  }
-  const data = await getData();
+  // async function getData() {
+  //   let lessons;
+  //   try {
+  //     lessons = await prisma.lesson.findMany();
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   return lessons;
+  // }
+  // const data = await getData();
   return (
     <div className="flex flex-col h-full">
-      <NavForCreate />
-      <CreateContent lessonsToChooseFrom={data} />
+      {/* <NavForCreate />
+      <CreateContent lessonsToChooseFrom={data} /> */}
+      <h1>create content</h1>
     </div>
   );
 }
